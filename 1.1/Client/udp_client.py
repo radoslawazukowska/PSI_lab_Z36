065d35b994acfac46c0ec14bb83d4b3b98c4ebc9
radoslawazukowska <radoslawa.zuk@gmail.com>
2025-11-15 09:58:42 +0100

    putted socket in with statment added more messages

--- 1.1/Client/udp_client.py
+++ 1.1/Client/udp_client.py
@@ -6,6 +6,8 @@ HOST = '127.0.0.1'  # The server's hostname or IP address
 times = {}
 sizes = [2, 4, 8, 16, 32]
 
+print("This is UDP client!!!")
+
 # TODO Implement better way of finding what is the maximum size of datagram
 
 if  len(sys.argv) < 3: 
@@ -16,15 +18,20 @@ else:
     host = sys.argv[1]
     port = int( sys.argv[2] )
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+print('Starting experiment')
 
-for size in sizes:
-    try:
-        message = bytes()
-        start = time.process_time()
-        data = sock.sendto(message, (host, port))
-        recv = sock.recvfrom()
-        finish = time.process_time()
-        times[size] = (finish - start)
-    except Exception:
-        break
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+    for size in sizes:
+        try:
+            message = b"x" * size
+            start = time.perf_counter()
+            s.sendto(message, (host, port))
+            data, addr = s.recvfrom(4096)
+            finish = time.perf_counter()
+            times[size] = (finish - start)
+            print(f'For size of {size}B it took {time[size]}s to get answer')
+        except Exception:
+            print(f"Failed for {size}B")
+            break
+
+print('Client finished.')
