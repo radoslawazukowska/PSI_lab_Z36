7641d88e3a2a7e7c75d40bb3d58fba970bf0a0aa
szczypawkaaa <szczypawkaaa@gmail.com>
2025-11-20 01:51:20 +0100

    Improve udp_client

--- 1.1/Client/udp_client.py
+++ 1.1/Client/udp_client.py
@@ -2,38 +2,40 @@ import socket
 import sys
 import time
 
-HOST = '127.0.0.1'  # The server's hostname or IP address
+HOST = "127.0.0.1"  # The server's hostname or IP address
+BUFSIZE = 4096
 times = {}
-sizes = [60000, 65500, 65506, 65507, 65508]
+datagram_sizes = [60000, 65500, 65506, 65507, 65508]
 
-print("This is UDP client!!!")
+print("This is UDP client.")
 
 # TODO Implement better way of finding what is the maximum size of datagram
 
-if  len(sys.argv) < 3: 
+if len(sys.argv) < 3:
     print("no port and/or host, using localhost:5005")
-    port=8000
-    host=HOST
+    port = 8000
+    host = HOST
 else:
     host = sys.argv[1]
-    port = int( sys.argv[2] )
+    port = int(sys.argv[2])
 
-print('Starting experiment')
+print("Starting experiment")
 
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
-    s.bind(("0.0.0.0", 5000))
-    for size in sizes:
+    # s.bind(("0.0.0.0", 5000)) #UDP nie bindujemy
+    for size in datagram_sizes:
         try:
             message = b"x" * size
             start = time.perf_counter()
             s.sendto(message, (host, port))
-            data, addr = s.recvfrom(4096)
+            data = s.recv(BUFSIZE)
+            # zamiana na recv zgodnie z prezentacją, odbiera tylko 26 bajtów
             finish = time.perf_counter()
-            times[size] = (finish - start)
-            print(f'For size of {size}B it took {times[size]}s to get answer')
+            times[size] = finish - start
+            print(f"For datagram size of {size}B it took {times[size]}s to get answer")
         except Exception as e:
             print(f"Failed for {size}B")
             print(e)
             break
 
-print('Client finished.')
+print("Client finished.")
