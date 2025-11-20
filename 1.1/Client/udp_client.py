74a6f619c5a34d9fa09e4d5409d203cd2f43f7cf
szczypawkaaa <szczypawkaaa@gmail.com>
2025-11-20 19:58:35 +0100

    Refactore code

--- 1.1/Client/udp_client.py
+++ 1.1/Client/udp_client.py
@@ -1,56 +1,64 @@
 import socket
 import sys
 import time
+import json
 
-HOST = "127.0.0.1"  # The server's hostname or IP address
+HOST = "127.0.0.1"
 BUFSIZE = 4096
 START_SIZE = 2
+# PORT = 8000
+INCR = "mul"
 
 actions = {
-    "increment": lambda x: x + 1,
+    "inc": lambda x: x + 1,
     "add100": lambda x: x + 100,
     "add1000": lambda x: x + 1000,
     "mul": lambda x: x * 2,
 }
-action = "mul"
 
-times = []
-dgram_sizes = []
 
-print("This is UDP client.")
+def save_times(filename, times, sizes):
+    with open(filename, "w") as f:
+        json.dump({"sizes": sizes, "times": times}, f)
 
-if len(sys.argv) < 3:
-    print("no port and/or host, using localhost:5005")
-    port = 8000
-    host = HOST
-else:
-    host = sys.argv[1]
-    port = int(sys.argv[2])
 
-print("Starting experiment")
+if __name__ == "__main__":
+    print("This is UDP client.")
 
-with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
-    size = START_SIZE
-    while True:
-        try:
-            message = b"x" * size
-            start = time.perf_counter()
-            s.sendto(message, (host, port))
-            data = s.recv(BUFSIZE)
+    if len(sys.argv) < 3:
+        print("no port and/or host, using localhost:5005")
+        port = 8000
+        host = HOST
+    else:
+        host = sys.argv[1]
+        port = int(sys.argv[2])
 
-            finish = time.perf_counter()
-            duration = finish - start
+    start_size = START_SIZE
+    incr_action = actions[INCR]
 
-            print(f"For datagram size of {size}B it took {duration}s to get answer")
+    times = []
+    dgram_sizes = []
 
-            times.append(duration)
-            dgram_sizes.append(size)
-            size = actions[action](size)
-        except Exception as e:
-            print(f"Failed for {size}B")
-            print(e)
-            break
+    print("Starting experiment")
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+        size = start_size
+        while True:
+            try:
+                message = b"x" * size
+                start = time.perf_counter()
+                s.sendto(message, (host, port))
+                data = s.recv(BUFSIZE)
+                duration = time.perf_counter() - start
+                print(f"For datagram size of {size}B it took {duration}s to get answer")
 
-print("Client finished.")
-print(dgram_sizes)
-print(times)
+                times.append(duration)
+                dgram_sizes.append(size)
+                size = incr_action(size)
+
+            except Exception as e:
+                print(f"Failed for {size}B")
+                print(e)
+                break
+    print("Client finished.")
+
+    save_times("z36_dgram_times.json", times, dgram_sizes)
