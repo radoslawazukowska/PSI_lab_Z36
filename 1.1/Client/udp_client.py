538b45da74ab5268904e21774383a8f7e8dbd779
szczypawkaaa <szczypawkaaa@gmail.com>
2025-11-20 02:43:31 +0100

    Add more sizes to client

--- 1.1/Client/udp_client.py
+++ 1.1/Client/udp_client.py
@@ -4,8 +4,10 @@ import time
 
 HOST = "127.0.0.1"  # The server's hostname or IP address
 BUFSIZE = 4096
-times = {}
-datagram_sizes = [60000, 65500, 65506, 65507, 65508]
+# times = {}
+# datagram_sizes = [60000, 65500, 65506, 65507, 65508]
+times = []
+dgram_sizes = []
 
 print("This is UDP client.")
 
@@ -23,7 +25,11 @@ print("Starting experiment")
 
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
     # s.bind(("0.0.0.0", 5000)) #UDP nie bindujemy
-    for size in datagram_sizes:
+    # for size in datagram_sizes:
+    size = 1
+    while True:
+        size *= 2
+        dgram_sizes.append(size)
         try:
             message = b"x" * size
             start = time.perf_counter()
@@ -31,11 +37,14 @@ with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
             data = s.recv(BUFSIZE)
             # zamiana na recv zgodnie z prezentacją, odbiera tylko 26 bajtów
             finish = time.perf_counter()
-            times[size] = finish - start
-            print(f"For datagram size of {size}B it took {times[size]}s to get answer")
+            # times[size] = finish - start
+            times.append(finish - start)
+            print(f"For datagram size of {size}B it took {times[-1]}s to get answer")
         except Exception as e:
             print(f"Failed for {size}B")
             print(e)
             break
 
 print("Client finished.")
+print(dgram_sizes)
+print(times)
